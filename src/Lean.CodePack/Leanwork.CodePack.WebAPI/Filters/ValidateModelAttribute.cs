145ae45e7956ec07d68282634e33d2cd3f36f46b
Michel Banagouro <michel@leanwork.com.br>
2015-12-02 16:05:19 -0200

    Adicionado novo pacote de código para Web API

---
+++ src/Lean.CodePack/Leanwork.CodePack.WebAPI/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Leanwork.CodePack.WebAPI.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext == null)
+            {
+                throw new ArgumentNullException("actionContext");
+            }
+            if (!actionContext.ModelState.IsValid)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, actionContext.ModelState);
+            }
+            base.OnActionExecuting(actionContext);
+        }
+    }
+}
