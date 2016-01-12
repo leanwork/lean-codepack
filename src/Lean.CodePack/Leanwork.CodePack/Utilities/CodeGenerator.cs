3e69872e1b9ea24bd414fdcabd8cb18414803fe8
rickreis <henriquevdosreis@hotmail.com>
2016-01-12 14:06:55 -0200

    add impl codegenerator

---
+++ src/Lean.CodePack/Leanwork.CodePack/Utilities/CodeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Leanwork.CodePack.Utilities
+{
+    public class CodeGenerator
+    {
+        const string ALPHABET = "AG8FOLE2WVT1CPY5ZH3NIUDBXSMQK7946";
+
+        public static string New(int lenght = 6)
+        {
+            Random random = new Random();
+
+            char[] keys = ALPHABET.ToCharArray();
+
+            return Enumerable
+                .Range(1, lenght) // for(i.. ) 
+                .Select(k => keys[random.Next(0, keys.Length - 1)])  // generate a new random char 
+                .Aggregate("", (e, c) => e + c); // join into a string
+        }
+    }
+}
