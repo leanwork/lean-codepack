e00439a38c79d9ad79bfd500ed90690a44c13603
rickreis <henriquevdosreis@hotmail.com>
2016-02-26 17:14:11 -0300

    modelbinder datetime and refactoring

--- src/Lean.CodePack/Leanwork.CodePack.Mvc/ModelBinders/DateTimeModelBinder.cs
+++ src/Lean.CodePack/Leanwork.CodePack.Mvc/ModelBinders/DateTimeModelBinder.cs
@@ -3,11 +3,11 @@ using System.Globalization;
 
 namespace Leanwork.CodePack.Mvc
 {
-    public class DecimalModelBinder : ModelBinder
+    public class DateTimeModelBinder : ModelBinder
     {
         public override object ConvertTo(string value)
         {
-            return Convert.ToDecimal(value, CultureInfo.CurrentCulture);
+            return Convert.ToDateTime(value, CultureInfo.CurrentCulture);
         }
     }
 }
---
+++ src/Lean.CodePack/Leanwork.CodePack.Mvc/ModelBinders/ModelBinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Leanwork.CodePack.Mvc
+{
+    public abstract class ModelBinder : IModelBinder
+    {
+        public abstract object ConvertTo(string value);
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var valueResult =
+                bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            var modelState = new ModelState { Value = valueResult };
+
+            object actualValue = null;
+
+            try
+            {
+                actualValue = ConvertTo(valueResult.AttemptedValue);
+
+                return ConvertTo(valueResult.AttemptedValue);
+            }
+            catch (FormatException e)
+            {
+                modelState.Errors.Add(e);
+            }
+
+            if (!bindingContext.ModelState.ContainsKey(bindingContext.ModelName))
+            {
+                bindingContext.ModelState.Add(bindingContext.ModelName, modelState);
+            }   
+
+            return actualValue;            
+        }        
+    }
+}
