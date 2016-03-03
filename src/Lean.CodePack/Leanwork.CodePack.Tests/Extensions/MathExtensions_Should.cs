3eb90dc02b61e31dca6c7c5b9a72cc3194971779
Michel Banagouro <michel@leanwork.com.br>
2016-03-03 14:00:56 -0300

    Adicionado extensões de cálculos matemáticos

---
+++ src/Lean.CodePack/Leanwork.CodePack.Tests/Extensions/MathExtensions_Should.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leanwork.CodePack;
+
+namespace Leanwork.CodePack.Tests.Extensions
+{
+    [TestClass]
+    public class MathExtensions_Should
+    {
+        [TestMethod]
+        public void Returns_percentage_amount()
+        {
+            //arrange
+            int total = 120;
+            int amount = 60;
+
+            //act
+            decimal expected = 50;
+            decimal result = total.Percentage(amount);
+
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Returns_percentage_amount_equals_zero()
+        {
+            //arrange
+            int total = 0;
+            int amount = 60;
+
+            //act
+            decimal expected = 0;
+            decimal result = total.Percentage(amount);
+
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
