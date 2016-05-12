4fb7c7fc1d10890dee45bd213f35dafdc107dac3
rickreis <henriquevdosreis@hotmail.com>
2016-05-12 11:13:51 -0300

    created leanwork codepack mail sender

---
+++ src/Lean.CodePack/Leanwork.CodePack.Mail/ActionMailer/Mailing/IMailer.cs
@@ -0,0 +1,64 @@
+﻿using ActionMailer.Net.Standalone;
+using System;
+using System.Threading.Tasks;
+
+namespace Leanwork.CodePack.Mail
+{
+    public interface IMailer
+    {
+        void SendEmail(MailDTO model);
+    }
+
+    public class MailerSend : RazorMailerBase, IMailer
+    {
+        object _model;
+        string _template;
+
+        public override string ViewPath
+        {
+            get { return "Templates\\Mailing"; }
+        }
+
+        public MailerSend(string template, object model)
+        {
+            _template = template;
+            _model = model;
+        }
+
+        public void SendEmail(MailDTO model)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Subject = model.Subject;
+
+                this.SetEmailValues(model);                
+
+                RazorEmailResult result = Email(_template, _model);
+
+                if (null == result)
+                {
+                    return;
+                }
+
+                result.DeliverAsync();
+            });            
+        }
+
+        private void SetEmailValues(MailDTO mail)
+        {
+            if (null == mail)
+            {
+                return;
+            }
+
+            //email from
+            From = String.Format("{0} <{1}>", mail.SenderName, mail.SenderEmail);
+
+            // email to
+            foreach (var receiver in mail.Receivers)
+            {
+                To.Add(receiver);
+            }
+        }        
+    }
+}
---
+++ src/Lean.CodePack/Leanwork.CodePack.Mail/ActionMailer/Mailing/MailDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Leanwork.CodePack.Mail
+{
+    public class MailDTO
+    {
+        public string SenderName { get; set; }
+
+        public string SenderEmail { get; set; }                
+
+        public string Subject { get; set; }                
+
+        public IList<string> Receivers
+        {
+            get { return _receivers ?? (_receivers = new List<string>()); }
+            set { _receivers = value; }
+        }
+        IList<string> _receivers;        
+    }    
+}
