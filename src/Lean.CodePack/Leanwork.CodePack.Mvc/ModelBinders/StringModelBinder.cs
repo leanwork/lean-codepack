7acaaf766c77ac7230cc310256b2e1994fd9bd05
Fabio Santos <fabiorsj@gmail.com>
2016-10-05 14:25:04 -0300

    Adicionado StringModelBinder - Aplica Trim() em todas as strings no bind

---
+++ src/Lean.CodePack/Leanwork.CodePack.Mvc/ModelBinders/StringModelBinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Leanwork.CodePack.Mvc
+{
+    public class StringModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext,
+        ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (valueResult == null || valueResult.AttemptedValue == null)
+                return null;
+            else if (valueResult.AttemptedValue == string.Empty)
+                return string.Empty;
+            return valueResult.AttemptedValue.Trim();
+        }
+    }
+}
