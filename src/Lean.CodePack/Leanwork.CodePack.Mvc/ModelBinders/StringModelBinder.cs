5298a1ddc69f24b5c338a062ec3357388241c0d3
Fabio Santos <fabiorsj@gmail.com>
2016-10-10 09:07:44 -0300

    Correção para aceitar conteúdo HTML na string

--- src/Lean.CodePack/Leanwork.CodePack.Mvc/ModelBinders/StringModelBinder.cs
+++ src/Lean.CodePack/Leanwork.CodePack.Mvc/ModelBinders/StringModelBinder.cs
@@ -9,15 +9,31 @@ namespace Leanwork.CodePack.Mvc
 {
     public class StringModelBinder : IModelBinder
     {
-        public object BindModel(ControllerContext controllerContext,
-        ModelBindingContext bindingContext)
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
-            ValueProviderResult valueResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            var shouldPerformRequestValidation =
+                controllerContext.Controller.ValidateRequest
+                && bindingContext.ModelMetadata.RequestValidationEnabled;
+
+            var valueResult = GetValueFromValueProvider(bindingContext, shouldPerformRequestValidation);
             if (valueResult == null || valueResult.AttemptedValue == null)
+            {
                 return null;
+            }
             else if (valueResult.AttemptedValue == string.Empty)
+            {
                 return string.Empty;
+            }
+
             return valueResult.AttemptedValue.Trim();
+        }
+
+        private ValueProviderResult GetValueFromValueProvider(ModelBindingContext bindingContext, bool performRequestValidation)
+        {
+            var unvalidatedValueProvider = bindingContext.ValueProvider as IUnvalidatedValueProvider;
+            return (unvalidatedValueProvider != null)
+                       ? unvalidatedValueProvider.GetValue(bindingContext.ModelName, !performRequestValidation)
+                       : bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
         }
     }
 }
