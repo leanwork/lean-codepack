a3b7096140c93651d249e17cc02e9893ea990222
Michel Banagouro <mbanagouro@outlook.com>
2017-03-21 15:28:21 -0300

    Adicionado GzipCompressionAttribute para WebAPI

---
+++ src/Lean.CodePack/Leanwork.CodePack.WebAPI/Filters/GzipCompressionAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace Leanwork.CodePack.WebAPI.Filters
+{
+    public class GzipCompressionAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionContext)
+        {
+            var content = actionContext.Response.Content;
+            var bytes = content == null ? null : content.ReadAsByteArrayAsync().Result;
+            var zlibbedContent = bytes == null ? new byte[0] :
+            CompressionHelper.GzipByte(bytes);
+            actionContext.Response.Content = new ByteArrayContent(zlibbedContent);
+            actionContext.Response.Content.Headers.Remove("Content-Type");
+            actionContext.Response.Content.Headers.Add("Content-encoding", "gzip");
+            actionContext.Response.Content.Headers.Add("Content-Type", "application/json");
+            base.OnActionExecuted(actionContext);
+        }
+    }
+
+    public class CompressionHelper
+    {
+        public static byte[] GzipByte(byte[] str)
+        {
+            if (str == null)
+            {
+                return null;
+            }
+
+            using (var output = new MemoryStream())
+            {
+                using (var compressor = new Ionic.Zlib.GZipStream(output, Ionic.Zlib.CompressionMode.Compress, Ionic.Zlib.CompressionLevel.BestSpeed))
+                {
+                    compressor.Write(str, 0, str.Length);
+                }
+                return output.ToArray();
+            }
+        }
+    }
+}
